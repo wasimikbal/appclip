9ca2f2f9e7b5886e48d039dade6a10f349cdd655
Waseem Iqbal <wasimiqbalyousafzai@gmail.com>
2024-10-07 22:18:56 +0500

    modified the ViewController bridge

--- ios/Navigate/ViewController.m
+++ ios/Navigate/ViewController.m
@@ -1,11 +1,6 @@
-//
-//  ViewController.m
-//  Navigate
-//
-//  Created by MacBook Pro  on 07/10/2024.
-//
-
 #import "ViewController.h"
+#import <React/RCTBundleURLProvider.h>
+#import <React/RCTRootView.h>
 
 @interface ViewController ()
 
@@ -13,10 +8,28 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+- (void)loadView {
+  #if DEBUG
+  // For DEBUG configuration, javascript will be loaded from index file
+  NSURL *jsCodeLocation = [[RCTBundleURLProvider sharedSettings]
+jsBundleURLForBundleRoot:@"index"];
+  #else
+  // For RELEASE configuration, js code is bundled and main.jsbundle file is created
+  NSURL *jsCodeLocation = [[NSBundle mainBundle]
+URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
+
+  // Value of moduleName should be equal to appName value set in index file
+  RCTRootView *rootView = [[RCTRootView alloc]
+initWithBundleURL:jsCodeLocation moduleName:@"ArticleApp"
+initialProperties:nil launchOptions:nil];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f
+green:1.0f blue:1.0f alpha:1];
+  self.view = rootView;
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
 
 @end
